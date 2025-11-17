d978a722da58697b4c57156ccb2ee37601e2008e
Rohan Rathod <rohangr7777@gmail.com>
2025-11-17 18:19:12 +0530

    Delete login.js

--- login.js
+++
@@ -1,8 +0,0 @@
-// Get a reference to the button element
-const nextPageButton = document.getElementById('signin');
-
-// Add an event listener for the 'click' event
-nextPageButton.addEventListener('click', function (e) {
-      // Change the window's location to the URL of the next page
-      window.location.href = 'login.html'; // Replace with your desired page
-});
